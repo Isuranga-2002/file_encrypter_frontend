0319f8827088384fcdfb0979582477fa62b227ad
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2026-06-17 09:06:53 +0530

    feat: added routes, configure typescript, added aws-amplify to dependencies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Upload } from './pages/upload/upload';
+import { Files } from './pages/files/files';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  { path: 'login', component: Login },
+
+  { path: 'register', component: Register },
+
+  { path: 'dashboard', component: Dashboard },
+
+  { path: 'upload', component: Upload },
+
+  { path: 'files', component: Files }
+];
